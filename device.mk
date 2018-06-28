4de919aa6a490ef2e8e06a62f8468f187bf211dc
Christopher N. Hesse <raymanfx@gmail.com>
2018-06-28 22:11:54 +0200

    enchilada: Initial commit
    Change-Id: I20117649a80db95fd3026058949e893af316351f

---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit from oneplus sdm845-common
+$(call inherit-product, device/oneplus/sdm845-common/common.mk)
+
+# Inherit from vendor blobs
+#$(call inherit-product, vendor/oneplus/enchilada/enchilada-vendor.mk)
