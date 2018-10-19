e35dfd72e7dc1e70afb4ec0719fc141d796fc8df
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    enchilada: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760

--- Android.mk
+++ Android.mk
@@ -28,5 +28,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),enchilada)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
