cc88abcdc221e6ae304c07583dd4fa31de3b196a
LuK1337 <priv.luk@gmail.com>
2019-02-01 11:50:02 +0100

    enchilada: Add notification LED related overlays
    Change-Id: I229b626136cc7c63f83436cd3cd1ba0e960a945e

--- device.mk
+++ device.mk
@@ -26,7 +26,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
