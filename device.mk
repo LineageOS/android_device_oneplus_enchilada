215eb4fc7ee99fb725deb9e6f1d590ca9256aafd
TH779 <i@779.moe>
2021-11-01 17:26:06 +0800

    enchilada: Drop duplicate config_deviceLightCapabilities overlay
     * Already defined in https://github.com/LineageOS/android_device_oneplus_sdm845-common/blob/c3e4dd2253e00206f961a901816b34594ba5c405/overlay-lineage/lineage-sdk/lineage/res/res/values/config.xml#L53-L94.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I7e6c2b45a2dac54d8bdcc2cca7bed11231be8cf9

--- device.mk
+++ device.mk
@@ -26,8 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
