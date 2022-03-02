1f8605a7fab6aef55c7723cb1d7388661204b595
Alexander Koskovich <akoskovich@pm.me>
2022-03-02 13:41:26 -0700

    enchilada: Set USB product string to 'OnePlus 6'
    * This is better for user facing stuff than 'ONEPLUS A6003'.
    
    Change-Id: I1f380699692c07b2fcc03fa83adc292b6d899cf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
