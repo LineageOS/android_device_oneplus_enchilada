5773779b17190ed7e1f79e62ea9e59c27b8b3c9f
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-05 22:30:09 -0400

    enchilada: Initial proprietary files list
    Change-Id: If9dfa5c8a9dfd2f6d7e0cfc78d4f193bb2266c48

--- device.mk
+++ device.mk
@@ -48,4 +48,4 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
 
 # Inherit from vendor blobs
-#$(call inherit-product, vendor/oneplus/enchilada/enchilada-vendor.mk)
+$(call inherit-product, vendor/oneplus/enchilada/enchilada-vendor.mk)
