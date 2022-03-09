181fd11422108a439ca9d00c586ac97621a8413b
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-08 22:25:39 -0400

    enchilada: Build lights HAL
    Change-Id: I6a2eb3c59e7cf5a6a74a128310737f81bdeedc93

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.oneplus_sdm845
+
 # Pocket mode
 PRODUCT_PACKAGES += \
     OnePlusPocketMode
