1c97469b4a76f110b9573ae5e1bdac8d3b6129d8
LuK1337 <priv.luk@gmail.com>
2022-03-10 23:07:13 +0100

    enchilada: Build fingerprint HAL and its deps
    Change-Id: I0e89fcab283b957b9d4262ae07e269cbc6dd433d

--- device.mk
+++ device.mk
@@ -40,6 +40,11 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service \
+    vendor.oneplus.fingerprint.extension@1.0.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.enchilada
