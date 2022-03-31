a8b71eeb3169bfd155ec31f8fe1fdc9ea336f6e7
LuK1337 <priv.luk@gmail.com>
2022-03-31 18:05:37 +0200

    Revert "enchilada: Make Android.mk search recursive"
    No longer needed.
    
    This reverts commit e35dfd72e7dc1e70afb4ec0719fc141d796fc8df.
    
    Change-Id: Iae9c4257a98dd4ba47ef9992b1dec133ccc56764

--- Android.mk
+++ Android.mk
@@ -28,6 +28,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),enchilada)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
