15e3b5e86351b886dbfef2c2730597b3c75dd591
LuK1337 <priv.luk@gmail.com>
2022-05-07 15:56:56 +0200

    enchilada: Import audio_policy_volumes.xml, default_volume_tables.xml
    Change-Id: I589d097af0a8d31df51bc55b07e7eb640dd99874

--- device.mk
+++ device.mk
@@ -40,6 +40,11 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
