f7bff7d14f2f42feddf5c90dbc82c0d3cb78539d
LuK1337 <priv.luk@gmail.com>
2022-06-16 10:01:05 +0200

    enchilada: Make NoCutoutOverlay overlay non-RRO
    Seems like our overlays get ignored otherwise.
    
    Change-Id: Ic03bac31922a57f548accd3c16417fa683565530

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/frameworks/base/packages/overlays/NoCutoutOverlay
+
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
