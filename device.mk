6467d6cef983064149f94bdcad9595f5c33affa6
LuK1337 <priv.luk@gmail.com>
2022-08-08 12:16:49 +0200

    enchilada: Ship FPC1020 blobs
    Also switch back to prebuilt fingerprint service because I don't really
    want to bother with adding non-FOD support in hardware/oneplus.
    
    Change-Id: I05d4a41ce370b44c4bb288a030f2ffe47110e857

--- device.mk
+++ device.mk
@@ -50,7 +50,6 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
     vendor.oneplus.fingerprint.extension@1.0.vendor
 
 # Lights
