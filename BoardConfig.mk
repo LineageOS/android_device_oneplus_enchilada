5395540d3ab99a7f2ce3503405922721c39687ae
Bruno Martins <bgcngm@gmail.com>
2022-09-19 11:34:34 +0100

    enchilada: Commonize bdroid_buildcfg
    Change-Id: I7b06908aa0f1c1012f77eb06049957bdcd6fcdff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@
 
 DEVICE_PATH := device/oneplus/enchilada
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
