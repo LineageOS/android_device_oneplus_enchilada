ac26ef665d48edfe526a7740faba6cbf9147e621
LuK1337 <priv.luk@gmail.com>
2024-06-24 11:51:05 +0200

    enchilada: Rename OOS fingerprint HAL
    Needed after BUILD_BROKEN_DUP_RULES was unset in sdm845-common.
    
    Change-Id: Iba6f764cdbe7141f0976cc71346e5ff2d7df7e66

--- device.mk
+++ device.mk
@@ -50,6 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.oneplus.fingerprint.extension@1.0.vendor
 
 # Lights
