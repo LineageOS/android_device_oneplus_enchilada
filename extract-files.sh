c662081cfb18d38a102dd7fc66e4189cd6bc1cb8
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    enchilada: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -17,5 +16,6 @@ set -e
 export DEVICE=enchilada
 export DEVICE_COMMON=sdm845-common
 export VENDOR=oneplus
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
 export DEVICE=enchilada
 export DEVICE_COMMON=sdm845-common
 export VENDOR=oneplus
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
