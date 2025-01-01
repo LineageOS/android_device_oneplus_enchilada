bd1dd6d83fb64ab588aafd0f484f8498bebbbe6a
Michael Bestas <mkbestas@lineageos.org>
2025-01-02 01:12:07 +0200

    enchilada: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- lineage_enchilada.mk
+++ lineage_enchilada.mk
@@ -16,6 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from enchilada device
